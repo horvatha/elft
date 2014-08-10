2758c4235c074723f43f04bde3d4692aa596cb9b
Arpad Horvath <horvath.arpad.szfvar@gmail.com>
2014-08-10 16:03:29 +0200

    Közelítés a PEP8-hoz

--- urls.py
+++ urls.py
@@ -2,20 +2,19 @@ from django.conf.urls.defaults import patterns, url
 from arekdjango.elft.models import Esemeny
 
 info_dict = {
-        'queryset': Esemeny.objects.all().order_by("-datum")
+    'queryset': Esemeny.objects.all().order_by("-datum")
 }
 
-urlpatterns = patterns('',
-
+urlpatterns = patterns(
+    '',
     (r'^$',  'arekdjango.elft.views.main'),
     (r'naptar/$',  'arekdjango.elft.views.main'),
-    #(r'^naptar/2013/$',  'django.views.generic.list_detail.object_list', info_dict),
     (r'^naptar/(?P<year>\d+)/$',  'arekdjango.elft.views.year'),
     (r'^esemeny/(?P<object_id>\d+)/$',  'arekdjango.elft.views.event'),
-    (r'^esemeny/(?P<object_id>\d+)/leiras$',  'arekdjango.elft.views.event_description'),
+    (r'^esemeny/(?P<object_id>\d+)/leiras$',
+        'arekdjango.elft.views.event_description'),
     (r'^szemely/(?P<object_id>\d+)/$',  'arekdjango.elft.views.szemely'),
     (r'^szemely/$',  'arekdjango.elft.views.szemelyek'),
     (r'^kategoria/(?P<object_id>\d+)/$',  'arekdjango.elft.views.category'),
     (r'^kategoria/$',  'arekdjango.elft.views.categories'),
 )
-
