a6221305dd11c96892edd0038a1b8983ae0bd369
Horvath Arpad <horvath.arpad.szfvar@gmail.com>
2014-08-11 02:03:24 +0200

    Nevek használata az url-eknél.

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls.defaults import patterns, url
-from arekdjango.elft.models import Esemeny
+from django.conf.urls import patterns, url
+from elft.models import Esemeny
 
 info_dict = {
     'queryset': Esemeny.objects.all().order_by("-datum")
@@ -7,14 +7,14 @@ info_dict = {
 
 urlpatterns = patterns(
     '',
-    (r'^$',  'arekdjango.elft.views.main'),
-    (r'naptar/$',  'arekdjango.elft.views.main'),
-    (r'^naptar/(?P<year>\d+)/$',  'arekdjango.elft.views.year'),
-    (r'^esemeny/(?P<object_id>\d+)/$',  'arekdjango.elft.views.event'),
-    (r'^esemeny/(?P<object_id>\d+)/leiras$',
-        'arekdjango.elft.views.event_description'),
-    (r'^szemely/(?P<object_id>\d+)/$',  'arekdjango.elft.views.szemely'),
-    (r'^szemely/$',  'arekdjango.elft.views.szemelyek'),
-    (r'^kategoria/(?P<object_id>\d+)/$',  'arekdjango.elft.views.category'),
-    (r'^kategoria/$',  'arekdjango.elft.views.categories'),
+    url(r'^$',  'elft.views.main', name='main'),
+    url(r'naptar/$',  'elft.views.main'),
+    url(r'^naptar/(?P<year>\d+)/$',  'elft.views.year', name='year'),
+    url(r'^esemeny/(?P<object_id>\d+)/$',  'elft.views.event', name='event'),
+    url(r'^esemeny/(?P<object_id>\d+)/leiras$',
+        'elft.views.event_description', name='event_description'),
+    url(r'^szemely/(?P<object_id>\d+)/$',  'elft.views.szemely', name='person'),
+    url(r'^szemely/$',  'elft.views.szemelyek', name='person_list'),
+    url(r'^kategoria/(?P<object_id>\d+)/$',  'elft.views.category', name='category'),
+    url(r'^kategoria/$',  'elft.views.categories', name='category_list'),
 )
